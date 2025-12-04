d5100208b961efb5384ceebf0d23d2209d219e2b
Jared Leonardo <dragcop19jad@gmail.com>
2025-12-04 11:52:34 -0600

    Cambio en la seguridad

--- src/main/java/com/example/marketing/Configuration/SecurityConfig.java
+++ src/main/java/com/example/marketing/Configuration/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable()) 
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/v1/auth/**").permitAll() 
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                .requestMatchers("/api/v1/analysis/**").permitAll() 
                 // .requestMatchers("/swagger-ui/**").permitAll()
                 // .requestMatchers("/swagger-ui.html").permitAll()
                 // .requestMatchers("/v3/api-docs/**").permitAll()
